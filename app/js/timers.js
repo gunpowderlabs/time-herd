5a5864b790a184768409da72e249647398cdebd8
Paweł Pierzchała <pawelpierzchala@gmail.com>
2015-07-13 12:01:51 +0200

    Sync timer with firebase

--- app/js/timers.js
+++ app/js/timers.js
@@ -1,20 +1,25 @@
 export class TimerController {
   constructor($interval, $firebaseObject, $scope) {
     var ref = new Firebase("https://shining-heat-7954.firebaseio.com/timer");
-    var syncTimer = $firebaseObject(ref);
+    this.timer = $firebaseObject(ref);
 
-    this.timer = {secondsLeft: 10, pause: false};
-    syncTimer.$bindTo($scope, "timer");
+    this.timer.secondsLeft = 10;
+    this.pause = false;
     this.$interval = $interval;
     this.start();
   }
   stop() {
     this.timer.pause = true;
+    this.timer.$save();
     this.$interval.cancel(this.tick);
   }
   start() {
     this.timer.pause = false;
+    this.timer.$save();
     if (this.timer.secondsLeft <= 0) { return; }
-    this.tick = this.$interval(() => this.timer.secondsLeft -= 1, 1000, this.timer.secondsLeft);
+    this.tick = this.$interval(() => {
+      this.timer.secondsLeft -= 1
+      this.timer.$save();
+    }, 1000, this.timer.secondsLeft);
   }
 }
